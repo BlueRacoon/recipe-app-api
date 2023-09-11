e2d81e7f6a56286ea1e5ebc593e11de372b1412d
Blue Raccoon <zachary_owen@aol.com>
2023-09-11 13:28:00 -0500

    issues with testing commands.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,11 @@
 """
 Django command to wait for the db to be available
 """
+import time
 
+from psycopg2 import OperationalError as Psycopg20pError
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -9,4 +13,15 @@ class Command(BaseCommand):
     """Django command to wait for db"""
 
     def handle(self, *args, **options):
-        pass
+        """Entry point for command."""
+        self.stdout.write('waiting for db...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
+                self.stbout.write('db unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('db available!'))
